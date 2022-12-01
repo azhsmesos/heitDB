07933e0416eb885715862dc9d4b04d83e796826a
zhaozhenhang <zhaozhenhang@kuaishou.com>
2022-12-01 20:07:13 +0800

    feat: implements Accept command line arguments

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+extern crate core;
+
+pub struct KvStore {}
+
+impl KvStore {
+    pub fn new() -> Self {
+        panic!()
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        panic!()
+    }
+
+    pub fn set(&self, key: String, val: String) {
+        panic!()
+    }
+
+    pub fn remove(&self, key: String, val: String) {
+        panic!()
+    }
+}
